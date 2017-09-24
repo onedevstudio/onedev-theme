99ce156b5fcadf4822be61cce4a9172cfb8fccae
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-24 19:39:32 -0300

    Added footer.php, header.php and analyticstracking.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The header for our theme.
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php wp_head(); ?>
+    <?php get_template_part( 'template-parts/analyticstracking' ) ?>
+</head>
+<body <?php body_class(); ?>>
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
+<?php get_header(); ?>
+
 <h1>Hello</h1>
+
+<?php get_footer();
