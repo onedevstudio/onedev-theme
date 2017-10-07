feba49812dcda442850f52820eb1645d26aaf0e9
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-06 21:49:56 -0300

    Added them setup, cleanup, enqueue scripts and google analytics scripts

--- functions.php
+++ functions.php
@@ -5,13 +5,21 @@
  * @since 1.0.0
  */
 
+$environment = ( defined('WP_DEBUG') && WP_DEBUG ) ? 'development' : 'production';
+
 /**
  *  Definitions
  */
- $the_theme = wp_get_theme();
- define( 'THEME_NAME', $the_theme['Name'] );
- define( 'THEME_VERSION', $the_theme['Version'] );
- define( 'THEME_FX', $the_theme['Name'] );
- define( 'THEME_PATH', get_template_directory() );
- define( 'THEME_URL', get_template_directory_uri() );
- define( 'SITE_URL', get_bloginfo( 'url' ) );
+$the_theme = wp_get_theme();
+define( 'THEME_ENV', $environment );
+define( 'THEME_NAME', $the_theme['Name'] );
+define( 'THEME_VERSION', $the_theme['Version'] );
+define( 'THEME_FX', $the_theme['Name'] );
+define( 'THEME_PATH', get_template_directory() );
+define( 'THEME_URL', get_template_directory_uri() );
+define( 'SITE_URL', get_bloginfo( 'url' ) );
+define( 'UA', 'UA-XXXXXXXXX-X' );
+
+require_once get_template_directory() . '/core/theme-setup.php';
+require_once get_template_directory() . '/core/theme-enqueue-scripts.php';
+require_once get_template_directory() . '/core/theme-cleanup.php';
